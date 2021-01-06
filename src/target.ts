d2b2cc53808fcae6cdbc0c05c21e434814ea6d2e
GwennLeCoin <gwennlecoin@gmail.com>
2021-01-06 08:31:17 +0100

    Add targets animation

--- src/target.ts
+++ src/target.ts
@@ -6,33 +6,47 @@ export class Target extends Entity {
 	valueScore: number
 	touchable: boolean
 	game: Game
+	animator: Animator
+	hitClip: AnimationState
 	
 	constructor(game: any, originSide: number,value:number){
 		super()
 		engine.addEntity(this)
+		
+		// Initialization
 		this.game = game
 		this.valueScore = value
+		this.addComponent(new GLTFShape('models/target1.glb'))
+		this.animator = new Animator()
+		this.addComponent(this.animator)
+		this.hitClip = new AnimationState('target1Hit')
+		this.hitClip.looping = false
+		this.hitClip.reset()
+		this.animator.addClip(this.hitClip)
+		
 		if(originSide < 5){
-			const StartPos = new Vector3(0,1,30)
-			const EndPos = new Vector3(15.5,1,30)
-			this.addComponent(new Transform({position: new Vector3(0.5,1,30), scale: new Vector3(1,2,0.2)}))
+			const StartPos = new Vector3(0,0,30)
+			const EndPos = new Vector3(15.5,0,30)
+			this.addComponent(new Transform({position: new Vector3(0.5,0,30), scale: new Vector3(1,1,1)}))
 			this.addComponent(new utils.MoveTransformComponent(StartPos, EndPos, 3))
 		} else {
-			const StartPos = new Vector3(15.5,1,30)
-			const EndPos = new Vector3(0,1,30)
-			this.addComponent(new Transform({position: new Vector3(15.5,1,30), scale: new Vector3(1,2,0.2)}))	
+			const StartPos = new Vector3(15.5,0,30)
+			const EndPos = new Vector3(0,0,30)
+			this.addComponent(new Transform({position: new Vector3(15.5,0,30), scale: new Vector3(1,1,1)}))	
 			this.addComponent(new utils.MoveTransformComponent(StartPos, EndPos, 3))
 		}
 		
 		this.addComponent(
 			new OnPointerDown((e) => {
 				log("myEntity was clicked", e)
+				this.hitClip.play()
 				this.game.score.addScore(this.valueScore)
-				this.removeComponent(utils.Delay)
-				engine.removeEntity(this)
+				this.addComponentOrReplace(new utils.Delay(3000, () =>{
+					// this.removeComponent(utils.Delay)
+					engine.removeEntity(this)
+				}))
 			}, {distance: 35})
 		)
-		this.addComponent(new BoxShape())
 		this.addComponent(new utils.Delay(3000, () =>{
 			log('perdu')
 			engine.removeEntity(this)
